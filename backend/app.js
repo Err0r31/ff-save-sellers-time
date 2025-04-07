bd8e747f41000b8186f6b1a0109ee8564bf36203
Err0r31 <egordem@list.ru>
2025-04-07 19:32:52 +0300

    prepare for vercel

--- backend/app.js
+++ backend/app.js
@@ -5,26 +5,29 @@ import authRouter from "./routes/auth.js";
 import categoriesRouter from "./routes/categories.js";
 import citiesRouter from "./routes/cities.js";
 import sequelize from "./database.js";
-import User from "./models/User.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
+
 app.use(express.json());
 
 app.use("/auth", authRouter);
 app.use("/categories", categoriesRouter);
 app.use("/cities", citiesRouter);
 
+app.get("/admin", (req, res) => {
+  res.send("Панель администратора");
+});
+
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
-});
-
-app.get("/admin", (req, res) => {
-  res.send("Панель администратора");
 });

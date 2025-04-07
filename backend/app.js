756da088952350c762e4b145b1a884ffacc93b76
Err0r31 <egordem@list.ru>
2025-04-07 20:55:42 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: "https://save-sellers-time.onrender.com/",
+  origin: "https://save-sellers-time.onrender.com",
   credentials: true,
 }));
 

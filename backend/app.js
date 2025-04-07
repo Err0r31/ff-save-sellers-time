72beccbb5a3f834f18d0b0b7fed1a5202d1fbfd5
Err0r31 <egordem@list.ru>
2025-04-07 20:42:57 +0300

    CORS for front

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(cors());
-}
+app.use(cors({
+  origin: "https://save-sellers-time.onrender.com/",
+  credentials: true,
+}));
 
 app.use(express.json());
 
